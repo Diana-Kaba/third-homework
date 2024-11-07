7ec68945ca1fb2cab29f743e41adf911b049f27b
Diana-Kaba <kabadiana327@gmail.com>
2024-11-07 12:14:09 +0200

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'third-homework';
+  arrOfStr: string[] = ['Paragraph 1', 'Paragraph 2', 'Paragraph 3', 'Paragraph 4'];
+  template = 'Template is empty';
+  selectedColor = 'lightgray';
+  isVisible = true;
+  test1 = 'Hi';
+  test2 = 'Test all pipes';
+
+  getContent(str: string) {
+    this.template = str;
+  }
+
+  onColorChange(color: string) {
+    this.selectedColor = color;
+  }
+
+  deleteElem() {
+    this.isVisible = false;
+  }
+
+  test() {
+    this.test2 = 'Event test';
+  }
 }
